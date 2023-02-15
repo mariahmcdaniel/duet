6f254f589d9fabd808d23f1bcf291d82bcf0e22f
A Stone <simplererstone@gmail.com>
2023-02-15 15:47:39 -0500

    updated mutations and queries on client side

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,25 +1,81 @@
 import { gql } from '@apollo/client';
 
 export const LOGIN_USER = gql`
-  mutation login($email: String!, $password: String!) {
-    login(email: $email, password: $password) {
-      token
-      user {
-        _id
-        username
-      }
+  mutation Login($username: String!, $password: String!) {
+  login(username: $username, password: $password) {
+    token
+    user {
+      _id
+      username
     }
   }
+}
 `;
 
 export const ADD_USER = gql`
-  mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(username: $username, email: $email, password: $password) {
-      token
-      user {
+  mutation AddUser($email: String!, $username: String!, $password: String!, $dateOfBirth: String, $city: String, $state: String, $interestedIn: String) {
+  addUser(email: $email, username: $username, password: $password, dateOfBirth: $dateOfBirth, city: $city, state: $state, interestedIn: $interestedIn) {
+    token
+    user {
+      _id
+      username
+    }
+  }
+}
+`;
+
+export const UPDATE_ANSWERS = gql`
+mutation UpdateAnswers($songAnswers: SongAnswersInput!) {
+  updateAnswers(songAnswers: $songAnswers) {
+    _id
+    username
+  }
+}
+`;
+
+export const UPDATE_PLAYLIST = gql`
+  mutation UpdatePlaylist($playlistAnswers: PlaylistAnswersInput!) {
+  updatePlaylist(playlistAnswers: $playlistAnswers) {
+    _id
+    username
+  }
+}
+`;
+
+export const CREATE_MATCH = gql`
+  mutation CreateMatch($userId: ID!) {
+  createMatch(userId: $userId) {
+    _id
+    status
+    receiver {
+      _id
+      username
+    }
+    sender {
+      _id
+      username
+    }
+    messages {
+      createdAt
+      sender {
         _id
         username
       }
+      text
     }
   }
+}
+`;
+
+export const CREATE_MESSAGE = gql`
+mutation CreateMessage($matchId: ID!, $text: String!) {
+  createMessage(matchId: $matchId, text: $text) {
+    createdAt
+    sender {
+      _id
+      username
+    }
+    text
+  }
+}
 `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,41 +1,203 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_USERS = gql`
-  query users {
-    users {
-      _id
-      username
-      email
-    }
-  }
-`;
-
-export const SEARCH_USERS = gql`
-  query searchUsers($term: String!) {
-    searchUsers(term: $term) {
-      _id
-      username
-      email
-    }
-  }
-`;
-
 export const QUERY_USER = gql`
-  query user($id: ID!) {
-    user(id: $id) {
-      _id
-      username
-      email
+  query User($userId: ID!) {
+  user(id: $userId) {
+    _id
+    city
+    dateOfBirth
+    email
+    gender
+    interestedIn
+    password
+    playlistAnswers {
+      playlistquestion1
+      playlistquestion2
+      playlistquestion3
+    }
+    pronouns
+    songAnswers {
+      songquestion1 {
+        artist
+        songClip
+        track
+      }
+      songquestion2 {
+        artist
+        songClip
+        track
+      }
+      songquestion3 {
+        artist
+        songClip
+        track
+      }
+      songquestion4 {
+        artist
+        songClip
+        track
+      }
+      songquestion5 {
+        artist
+        songClip
+        track
+      }
+      songquestion6 {
+        artist
+        songClip
+        track
+      }
+      songquestion7 {
+        artist
+        songClip
+        track
+      }
+    }
+    state
+    username
+  }
+}
+`;
+
+export const QUERY_USERS = gql`
+  query Users {
+  users {
+    _id
+    city
+    dateOfBirth
+    email
+    gender
+    interestedIn
+    password
+    playlistAnswers {
+      playlistquestion1
+      playlistquestion2
+      playlistquestion3
+    }
+    pronouns
+    state
+    username
+    songAnswers {
+      songquestion1 {
+        artist
+        songClip
+        track
+      }
+      songquestion2 {
+        artist
+        songClip
+        track
+      }
+      songquestion3 {
+        artist
+        songClip
+        track
+      }
+      songquestion4 {
+        artist
+        songClip
+        track
+      }
+      songquestion5 {
+        artist
+        songClip
+        track
+      }
+      songquestion6 {
+        artist
+        songClip
+        track
+      }
+      songquestion7 {
+        artist
+        songClip
+        track
+      }
     }
   }
+}
 `;
 
 export const QUERY_ME = gql`
-  query me {
-    me {
-      _id
-      username
-      email
+  query Me {
+  me {
+    _id
+    city
+    dateOfBirth
+    email
+    gender
+    interestedIn
+    password
+    pronouns
+    state
+    username
+    playlistAnswers {
+      playlistquestion1
+      playlistquestion2
+      playlistquestion3
+    }
+    songAnswers {
+      songquestion1 {
+        artist
+        songClip
+        track
+      }
+      songquestion2 {
+        artist
+        songClip
+        track
+      }
+      songquestion3 {
+        artist
+        songClip
+        track
+      }
+      songquestion4 {
+        artist
+        songClip
+        track
+      }
+      songquestion5 {
+        artist
+        songClip
+        track
+      }
+      songquestion6 {
+        artist
+        songClip
+        track
+      }
+      songquestion7 {
+        artist
+        songClip
+        track
+      }
     }
   }
+}
+`;
+
+export const QUERY_MATCHES = gql`
+  query Matches {
+  matches {
+    _id
+    status
+    receiver {
+      _id
+      username
+    }
+    sender {
+      _id
+      username
+    }
+    messages {
+      createdAt
+      sender {
+        _id
+        username
+      }
+      text
+    }
+  }
+}
 `;
