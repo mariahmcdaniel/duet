e08dcf55173bfa9f051bae6565c4a18d9e4bcb75
Ryan Young <ryanbyoung1@gmail.com>
2023-02-15 10:36:25 -0500

    fixed typedefs and set up matches query and message mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -17,9 +17,12 @@ const resolvers = {
       }
       throw new AuthenticationError('You need to be logged in!');
     },
-    messages: async (_, args, context) => {
+    matches: async (_, args, context) => {
       if (context.user) {
-        return await Match.findOne({ _id: args.id })
+        return await Match.find({ 
+          $or: [{ sender: context.user._id },{ receiver: context.user._id }],
+          status: 1
+         })
           .populate('messages')
           .populate('sender')
           .populate('receiver')
@@ -52,11 +55,11 @@ const resolvers = {
     },
     // userId is person you(user) has clicked check
     createMatch: async (_, { userId }, context) => {
-      const match = await Match.findOne({ receiver: context.user._id })
+      const match = await Match.findOne({ receiver: context.user._id, sender: userId })
       if (match) {
 
         Match.findOneAndUpdate(
-          { receiver: context.user._id },
+          { receiver: context.user._id, sender: userId },
           { $set: { status: 1 } },
           { new: true })
       }
@@ -67,7 +70,13 @@ const resolvers = {
       });
     },
     createMessage: async (_, args, context) => {
-
+        if (context.user) {
+          const message = await Message.create({ sender: context.user._id, text: args.text });
+          const match = await Match.findOneAndUpdate({ _id: args.matchId }, { $push: { messages: message._id } });
+          return message;
+        } else {
+          throw new AuthenticationError('You must be logged in to send messages');
+        }
     }
   }
 };
