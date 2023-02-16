8418240c7ed50a1a096f1d2c429052dea3e1db7c
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-02-15 22:40:23 -0500

    nav

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.png';
 
 import Auth from '../../utils/auth';
 
@@ -11,26 +12,59 @@ function Navbar() {
   
   if (Auth.loggedIn()) {
     return (
-      <>
-        <Link to="/me">
-          {Auth.getProfile().data.username}'s profile
-        </Link>
-        <button onClick={logout}>
-          Logout
-        </button>
-      </>
+      // <>
+      //   <Link to="/" to="/me">
+      //     {Auth.getProfile().data.username}'s profile
+      //   </Link>
+      //   <button onClick={logout}>
+      //     Logout
+      //   </button>
+      // </>
+
+      <nav className="navbar navbar-expand-lg navbar-secondary bg-light">
+    <div className="container-fluid">
+      <Link to="/duet" className="navbar-brand">Duet</Link>
+      <div className="collapse navbar-collapse" id="navbarColor03">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/potentials" className="nav-link active">Potentials
+              <span className="visually-hidden">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/matches" className="nav-link">Matches</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/me" className="nav-link">My Profile</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/chat" className="nav-link">Chat</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav> 
     );
   }
   // If logged out show login controls
   return (
-    <>
-      <Link to="/login">
-        Login
-      </Link>
-      <Link to="/signup">
-        Signup
-      </Link>
-    </>
+
+    <nav className="navbar navbar-expand-lg navbar-secondary bg-light">
+    <div className="container-fluid">
+      <Link to="/duet" className="navbar-brand"><img src={logo}/></Link>
+      <div className="collapse navbar-collapse" id="navbarColor03">
+        <ul className="navbar-nav ml-auto">
+          <li className="nav-item">
+            <Link to="/potentials" className="nav-link active">Login
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/signup" className="nav-link">Sign Up</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav> 
   )
 }
 
